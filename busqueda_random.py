e4dcb2c4e951728805b5d4562c7a080900cbb6b2
nahuel-silva <nahuel_11_arg@hotmail.com>
2022-08-23 11:18:21 -0300

    salio :)

--- busqueda_random.py
+++ busqueda_random.py
@@ -14,111 +14,35 @@ class B_random():
                     pos_fila0 = pos_fila0 - 1
                     pos_cero = matriz_r[pos_fila0].index(0)     
                     return pos_fila0, pos_cero
+    
+    def movimientos_posibles(self, matriz):
+        
+        pos_fila0, pos_cero = B_random().posicion_cero(matriz)
 
-    def movimientos_posibles(self, matriz_r):
+        posiciones_dis = []
 
-        pos_fila0, pos_cero = B_random().posicion_cero(matriz_r)
+        cant_filas = len(matriz)
 
-        # Primera fila
-        if pos_fila0 == 0:
-            # Indice a11
-            if pos_cero == 0:
-                pos_mov = [0,1]
-                n_random = random.choice(pos_mov)
-                if n_random == 1:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[pos_fila0][n_random] = matriz_r[pos_fila0][n_random], matriz_r[pos_fila0][pos_cero]
-                elif n_random == 0:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[1][pos_cero] =  matriz_r[1][pos_cero], matriz_r[pos_fila0][pos_cero]
+        if (pos_fila0 + 1) < cant_filas:
+            posicion = ((pos_fila0+1) , pos_cero)
+            posiciones_dis.append(posicion)
+        if (pos_fila0 - 1) >= (cant_filas-cant_filas):
+            posicion = ((pos_fila0-1) , pos_cero)
+            posiciones_dis.append(posicion)
+        if (pos_cero + 1) < cant_filas:
+            posicion = (pos_fila0, (pos_cero+1))
+            posiciones_dis.append(posicion)
+        if (pos_cero - 1) >= (cant_filas-cant_filas):
+            posicion = (pos_fila0, (pos_cero-1))
+            posiciones_dis.append(posicion)
 
-            # Indice a12
-            elif pos_cero == 1:
-                pos_mov = [0,1,2]
-                n_random = random.choice(pos_mov)
-                if n_random == 0:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[pos_fila0][n_random] = matriz_r[pos_fila0][n_random], matriz_r[pos_fila0][pos_cero]
-                elif n_random == 2:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[pos_fila0][n_random] = matriz_r[pos_fila0][n_random], matriz_r[pos_fila0][pos_cero]
-                elif n_random == 1:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[1][n_random] = matriz_r[1][n_random], matriz_r[pos_fila0][pos_cero]
-            
-            # Indice a13
-            elif pos_cero == 2:
-                pos_mov = [1,2]
-                n_random = random.choice(pos_mov)
-                if n_random == 1:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[pos_fila0][n_random] = matriz_r[pos_fila0][n_random], matriz_r[pos_fila0][pos_cero]
-                elif n_random == 2:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[1][pos_cero] = matriz_r[1][pos_cero], matriz_r[pos_fila0][pos_cero]
+        pos_random = random.choice(posiciones_dis)
 
-        # Segunda fila
-        elif pos_fila0 == 1:
-            # Indice a21
-            if pos_cero == 0:
-                pos_mov = [0,1,2]
-                n_random = random.choice(pos_mov)
-                if n_random == 0:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[0][n_random] = matriz_r[0][n_random], matriz_r[pos_fila0][pos_cero]
-                elif n_random == 1:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[pos_fila0][n_random] = matriz_r[pos_fila0][n_random], matriz_r[pos_fila0][pos_cero]
-                elif n_random == 2:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[n_random][0] = matriz_r[n_random][0], matriz_r[pos_fila0][pos_cero]
+        for pos in posiciones_dis:
+            if pos_random == pos:
+                matriz[pos_fila0][pos_cero], matriz[pos_random[0]][pos_random[1]] = matriz[pos_random[0]][pos_random[1]], matriz[pos_fila0][pos_cero]
 
-            # Indice a22
-            elif pos_cero == 1:
-                pos_mov = [0,1,2,3]
-                n_random = random.choice(pos_mov)
-                if n_random == 0:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[pos_fila0][n_random] = matriz_r[pos_fila0][n_random], matriz_r[pos_fila0][pos_cero]
-                elif n_random == 1:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[0][n_random] = matriz_r[0][n_random], matriz_r[pos_fila0][pos_cero]
-                elif n_random == 2:
-                    matriz_r[pos_fila0][pos_cero],  matriz_r[pos_fila0][n_random] = matriz_r[pos_fila0][n_random], matriz_r[pos_fila0][pos_cero]
-                elif n_random == 3:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[2][pos_cero] = matriz_r[2][pos_cero], matriz_r[pos_fila0][pos_cero]
-            
-            # Indice a23
-            elif pos_cero == 2:
-                pos_mov = [0,1,2]
-                n_random = random.choice(pos_mov)
-                if n_random == 0:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[n_random][pos_cero] = matriz_r[n_random][pos_cero], matriz_r[pos_fila0][pos_cero]
-                elif n_random == 1:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[n_random][n_random] = matriz_r[n_random][n_random], matriz_r[pos_fila0][pos_cero]
-                elif n_random == 2:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[n_random][pos_cero] = matriz_r[n_random][pos_cero], matriz_r[pos_fila0][pos_cero]
-
-        # Tercera fila
-        elif pos_fila0 == 2:
-            # Indice a31
-            if pos_cero == 0:
-                pos_mov = [1,2]
-                n_random = random.choice(pos_mov)
-                if n_random == 1:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[n_random][pos_cero] = matriz_r[n_random][pos_cero], matriz_r[pos_fila0][pos_cero]
-                elif n_random == 2:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[pos_fila0][1] = matriz_r[pos_fila0][1], matriz_r[pos_fila0][pos_cero]
-
-            # Indice a32
-            elif pos_cero == 1:
-                pos_mov = [0,1,2]
-                n_random = random.choice(pos_mov)
-                if n_random == 0:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[pos_fila0][n_random] = matriz_r[pos_fila0][n_random], matriz_r[pos_fila0][pos_cero]
-                elif n_random == 1:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[n_random][pos_cero] = matriz_r[n_random][pos_cero], matriz_r[pos_fila0][pos_cero]
-                elif n_random == 2:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[pos_fila0][n_random] = matriz_r[pos_fila0][n_random], matriz_r[pos_fila0][pos_cero]
-
-            # Indice a33
-            elif pos_cero == 2:
-                pos_mov = [1,2]
-                n_random = random.choice(pos_mov)
-                if n_random == 1:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[pos_fila0][n_random] = matriz_r[pos_fila0][n_random], matriz_r[pos_fila0][pos_cero]
-                elif n_random == 2:
-                    matriz_r[pos_fila0][pos_cero], matriz_r[1][n_random] = matriz_r[1][n_random], matriz_r[pos_fila0][pos_cero]
-
-        return matriz_r
+        return matriz
 
     def definitiva(self, matriz_r, matriz_o):
 
@@ -128,8 +52,12 @@ class B_random():
             matriz_armada = B_random().movimientos_posibles(matriz_r)
             contador += 1
             if matriz_armada == matriz_o:
-                print(f"\nNumero de movimientos necesarios {contador}")
                 print(f"\nMatriz resultante: ")
                 for fila in matriz_armada:
                     print(fila)
+                print(f"Numero de movimientos necesarios {contador}")
                 break
+            
+        
+
+        
--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 from matriz import *
-from busqueda_random2 import *
+from busqueda_random import *
+from mezclar import *
 
 def main():
 
-    cant_col = 3
+    # cant_fil = int(input("Cuantas filas quiere? "))
+    # cant_col = int(input("Cuantas columnas quiere? "))
+    # while cant_col != cant_fil:
+    #     print("Se necesita que la matriz sea cuadrada ")
+    #     cant_col = int(input("Cuantas columnas quiere? "))
+    #     cant_fil = int(input("Cuantas filas quiere? "))
+    #     if cant_col == cant_fil:
+    #         cant_col = cant_col
+    #         cant_fil = cant_fil
+    #         break
+
     cant_fil = 3
-    num_mezcladas = int(input("Cuantas veces quiere mezclar las fichas? "))
+    cant_col = 3
+    num_mezcladas = int(input("Cuantas veces quiere mover las fichas para mezclar? "))
 
     matriz_o = Matriz().armar_matriz(cant_col, cant_fil)
+    matriz_o2 = Matriz().armar_matriz(cant_col, cant_fil)
 
-    B_random().movimientos_posibles(matriz_o)
+    matriz_r = Mezclar().mezclar_random(matriz_o, num_mezcladas)
+
+    B_random().definitiva(matriz_r, matriz_o2)
 
 if __name__ == '__main__':
     main()
