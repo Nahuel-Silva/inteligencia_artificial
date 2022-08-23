fb042b6844275e8752935abed5dd4559d6b3bfa8
nahuel <nahuel_11_arg@hotmail.com>
2022-08-23 09:31:23 -0300

    se hizo lo que se pudo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from matriz import *
-from busqueda_random import *
+from busqueda_random2 import *
 
 def main():
 
@@ -8,21 +8,8 @@ def main():
     num_mezcladas = int(input("Cuantas veces quiere mezclar las fichas? "))
 
     matriz_o = Matriz().armar_matriz(cant_col, cant_fil)
-    # matriz_o = Matriz().matriz_np(cant_col, cant_fil)
 
-    matriz_r = Matriz().matriz_random(cant_col, cant_fil, num_mezcladas)
-    # Matriz().random_np(cant_col, cant_fil, num_mezcladas)
-
-    print(f"Matriz deseada:" )
-    for fila in matriz_o:
-        print(fila)
-
-    print(f"\nMatriz random:" )
-    for fila in matriz_r:
-        print(fila)
-
-    B_random().definitiva(matriz_r, matriz_o)
-
+    B_random().movimientos_posibles(matriz_o)
 
 if __name__ == '__main__':
     main()
