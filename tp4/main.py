fefdeea48fc754df748fb09fb82d11fa411d0baa
nahuel <nahuel_11_arg@hotmail.com>
2022-10-14 19:51:27 -0300

    act

--- tp4/main.py
+++ tp4/main.py
@@ -7,20 +7,24 @@ def main():
 
     n = int(input("Cuantas neuronas quiere en la capa oculta (1 a n)? "))
 
-    pesos_ini = []
+    pesos_cpo = []
+
+    a = 0
 
     for i in range(n*3):
+        a += 1
         p = float(input(f"Ingrese el peso {i}: "))
-        pesos_ini.append(p)
+        pesos_cpo.append(p)
+
+    for i in range(n):
+        a += 1
+        p = float(input(f"Ingrese el peso {a}: "))
 
     lista_div = []
-    for i in range(0, len(pesos_ini), 3):
-        lista_div.append(pesos_ini[i:i+3])
+    for i in range(0, len(pesos_cpo), 3):
+        lista_div.append(pesos_cpo[i:i+3])
 
     lista_y = []
-    lista_wa = []
-    lista_wb = []
-    lista_wc = []
     
     while c != 1:
         c += 1
@@ -31,7 +35,7 @@ def main():
                 s_real_y = Capa_oculta().neurona_generica(i, j)
                 lista_y.append(s_real_y)
 
-    print(lista_y)
+    
 
         
 
