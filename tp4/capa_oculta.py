516184e67840ab4139333ec1b01100bbcc71a3ff
nahuel-silva <nahuel_11_arg@hotmail.com>
2022-11-07 18:43:42 -0300

    act

--- tp4/capa_oculta.py
+++ tp4/capa_oculta.py
@@ -7,9 +7,9 @@ class Capa_oculta():
         lista_cortada = list_pesos[1:]
         fila_cortada = fila[:-1]
         vias = 1
-        x = list_pesos[0]*vias
+        x = (list_pesos[0]*vias)
         for i in range(len(lista_cortada)):
-            x += (lista_cortada[i] + fila_cortada[i])
+            x += (lista_cortada[i] * fila_cortada[i])
         numerador = 1
         denominador = 1 + (math.e**(-x))
         s_real_y = numerador / denominador
