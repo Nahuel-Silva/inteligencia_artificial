d3f89c75b77a5b3a8e50d8a604d7e23092d99c42
nahuel-silva <nahuel_11_arg@hotmail.com>
2022-11-07 18:28:11 -0300

    act

--- tp6/leer_img.py
+++ tp6/leer_img.py
@@ -1,26 +1,26 @@
 import cv2
 import copy
-import os
 
 def read_images():
-    lista_1 = []
-    lista_2 = []
-    persona = True
+    imagenes = []
+    aux = []
+    a = True
     input_images_path = "C:/Users/nahue/OneDrive/Desktop/inteligencia_artificial/tp6/fotosIA/"
-
     for i in range(10):
         img = cv2.imread(input_images_path + str(i) + ".jpg", cv2.COLOR_RGB2GRAY)
-        for image in img:
-            for i in image:
-                for j in i:
-                    lista_1.append(j)
-                    # print(lista_1)
-        print(len(lista_1))
-        lista_1.clear()
-                    # lista_2.append(lista_1)
-        
-    # return lista_2
+        aux.clear()
+        for j in range(len(img)):
+            for k in range(len(img[0])):
+                px = img[j][k][0]
+                aux.append(px/255)
 
-if __name__ == '__main__':
-    read_images()
-    # print(len(a))
+        if a:
+            aux.append(int(0))
+            a = False
+        else:
+            aux.append(1)
+            a = True
+            
+        imagenes.append(copy.deepcopy(aux))
+
+    return imagenes
