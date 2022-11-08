5562a1681f3de894ee320f029b101e48dc492f7a
nahuel-silva <nahuel_11_arg@hotmail.com>
2022-11-08 20:25:16 -0300

    actualizacion mas generica

--- tp4/main.py
+++ tp4/main.py
@@ -13,25 +13,26 @@ def main():
 
     c = 0
 
-    tabla_xor = [[0, 0, 0],[0, 1, 1],[1, 0, 1],[1, 1, 0]]
+    tabla_xor = [[1, 0, 0, 0],[1, 0, 1, 1],[1, 1, 0, 1],[1, 1, 1, 0]]
 
     n = int(input("Cuantas neuronas quiere en la capa oculta (1 a n)? "))
+    cant_entradas = 3
 
     pesos_cpo = []
 
-    for i in range(n*3):
-        p = random.uniform(-1,1)
+    for i in range(n*cant_entradas):
+        p = random.uniform(-1, 1)
         pesos_cpo.append(p)
 
     pesos_ult_neu = []
 
     for i in range(n+1):
-        p = random.uniform(-1,1)
+        p = random.uniform(-1, 1)
         pesos_ult_neu.append(p)
 
     lista_div = []
-    for i in range(0, len(pesos_cpo), 3):
-        lista_div.append(pesos_cpo[i:i+3])
+    for i in range(0, len(pesos_cpo), cant_entradas):
+        lista_div.append(pesos_cpo[i:i+cant_entradas])
 
     lista_y = []
     
@@ -41,6 +42,7 @@ def main():
 
         print(f"--------------------------->Iteracion {c}<------------------------------------")
         for i in tabla_xor:
+            lista_y.append(1)
             for j in lista_div:
                 s_real_y = Capa_oculta().neurona_generica(i, j)
                 lista_y.append(s_real_y)
@@ -49,38 +51,35 @@ def main():
             pesos_ult_neu.clear()
             pesos_ult_neu = nuevas_w
 
-            if i == [0,0,0]:
+            if i == [1,0,0,0]:
                 lista_errores0.append(error)
-            elif i == [0,1,1]:
+            elif i == [1,0,1,1]:
                 lista_errores1.append(error)
-            elif i == [1,0,1]:
+            elif i == [1,1,0,1]:
                 lista_errores2.append(error)
-            elif i == [1,1,0]:
+            elif i == [1,1,1,0]:
                 lista_errores3.append(error)   
 
-            vias = 1
             lr = 0.5
 
             lista_comun = []
             
-            for l in range(len(lista_div)):
-                Soc_gen = lista_y[l]*(1-lista_y[l])*s
-                delta_prim = lr*vias*Soc_gen
-                delta_seg = lr*i[0]*Soc_gen
-                delta_ter = lr*i[1]*Soc_gen
-                w_prim = lista_div[l][0] + delta_prim
-                w_seg = lista_div[l][1] + delta_seg
-                w_ter = lista_div[l][2] + delta_ter
-                lista_comun.append(w_prim)
-                lista_comun.append(w_seg)
-                lista_comun.append(w_ter)
+            i_cortada = i[:-1]
+            y_cortada = lista_y[1:] 
+
+            for l in range(len(y_cortada)):
+                Soc_gen = y_cortada[l]*(1-y_cortada[l])*s
+                for q in range(len(i_cortada)):
+                    delta_gen = lr*i_cortada[q]*Soc_gen
+                    w_gen = lista_div[l][q] + delta_gen
+                    lista_comun.append(w_gen)
 
             lista_y.clear()
             lista_div.clear()
 
 
-            for f in range(0, len(lista_comun), 3):
-                lista_div.append(lista_comun[f:f+3])
+            for f in range(0, len(lista_comun), cant_entradas):
+                lista_div.append(lista_comun[f:f+cant_entradas])
 
     plt.plot(lista_contador, lista_errores0, label="errores0")
     plt.plot(lista_contador, lista_errores1, label="errores1")
